ce4c23a29c5fa97f52c824eed165dc872ac83cbf
Evan Tann <egtann@gmail.com>
2023-05-24 15:35:58 -0700

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "convert a terrafirma inventory.json file to ips for a service";
+
+  # Nixpkgs / NixOS version to use.
+  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
+
+  outputs = { self, nixpkgs }:
+    let
+
+      # to work with older version of flakes
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+
+      version = builtins.substring 0 8 lastModifiedDate;
+
+      # System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+
+      # Provide some binary packages for selected system types.
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          sqlc = pkgs.buildGoModule {
+              name = "sqlc";
+              src = ./.;
+              CGO_ENABLED = 0;
+              doCheck = false;
+              subPackages = [ "cmd/sqlc" ];
+              vendorSha256 = "QFxSUsA5FDEBr8z0DO09sdXclY9JBU9pfKfs0RH4Bjk=";
+            };
+        });
+
+      # The default package for 'nix build'. This makes sense if the
+      # flake provides only one package or there is a clear "main"
+      # package.
+      defaultPackage = forAllSystems (system: self.packages.${system}.sqlc);
+    };
+}
