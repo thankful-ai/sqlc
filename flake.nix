876cc19d6de4eb128f1af84b693712590013a7dd
Evan Tann <evan.tann@gladly.com>
2023-07-12 08:09:38 -0700

    add aarch64 flake

--- flake.nix
+++ flake.nix
@@ -5,18 +5,39 @@
     };
 
     outputs = {self, nixpkgs}: {
+      defaultPackage.aarch64-darwin =
+        with import nixpkgs { system = "aarch64-darwin"; };
+
+        stdenv.mkDerivation rec {
+          name = "sqlc-${version}";
+
+          version = "1.19.0";
+
+          # https://nixos.wiki/wiki/Packaging/Binaries
+          src = pkgs.fetchurl {
+            url = "https://github.com/kyleconroy/sqlc/releases/download/v1.19.0/sqlc_1.19.0_darwin_arm64.tar.gz";
+            sha256 = "1vq5s5xa6lcymacc01mhypwskyja6k1hqd3lv6frn5i92bs1437z";
+          };
+
+          sourceRoot = ".";
+
+          installPhase = ''
+            install -m755 -D sqlc $out/bin/sqlc
+          '';
+        };
+
       defaultPackage.x86_64-linux =
         with import nixpkgs { system = "x86_64-linux"; };
 
         stdenv.mkDerivation rec {
           name = "sqlc-${version}";
 
-          version = "1.18.0";
+          version = "1.19.0";
 
           # https://nixos.wiki/wiki/Packaging/Binaries
           src = pkgs.fetchurl {
-            url = "https://github.com/kyleconroy/sqlc/releases/download/v1.18.0/sqlc_1.18.0_linux_amd64.tar.gz";
-            sha256 = "sha256-PPttgt7JvBWjf5g4hss9VerNMbrYU43IZEl8/1nRM8U=";
+            url = "https://github.com/kyleconroy/sqlc/releases/download/v1.19.0/sqlc_1.19.0_linux_amd64.tar.gz";
+            sha256 = "1ff7ys9p9bjw17dn83v0s55hqfpv2lh9nxdb738kf75w06rwfjrb";
           };
 
           sourceRoot = ".";
